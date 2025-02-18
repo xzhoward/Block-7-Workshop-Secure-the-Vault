ec6cf12c5179ad7d53e8aa400d6b7adc0a90a99f
Howard Xu <xzhoward6@gmail.com>
2025-02-18 13:45:14 -0500

    Initial commit or updated changes

---
+++ script.js
@@ -0,0 +1,14 @@
+// Compose a message for the user
+const message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+// Get three variables using different arithmetic operators
+let num1 = 60 - 50;
+
+let num2 = 5 * 8;
+
+let num3 = 78 / 2;
+
+// Display the vault combination when the button is clicked
+function openVault() {
+    alert(`${message} \n${num1} - ${num2} - ${num3}`);
+}
